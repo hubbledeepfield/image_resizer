1e9a68a84b85f0543ecb039e5b8bd864251ffc0a
Vadym Rudenko <vadym.rudenko@gmail.com>
2014-11-26 19:31:35 -0800

    initial commit

---
+++ image_resizer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ue
+
+[ $# -eq 1 ] || { echo "Usage $0 <directory>"; exit 1; }
+
+#1. Resize each picture in a given directory.
+#2. Rename each picture with "$directory_name + $counter".
+#3. Several scale options.
+#4. Separate vertical and horizontal pictures.
+#5. Store resized files in a new directory called like "$directory_name + resized".
+#6. Progress bar.
+#7. Info messages.
+
+sips=/usr/bin/sips
+orig_dir="$1"
+res_dir=$( basename "${orig_dir}"_resized )
+
+echo ${res_dir}
+
+[ -d ${res_dir} ] || mkdir ${res_dir}
+
+picture_width=$( ${sips} -g "pixelWidth" ${orig_dir}/* | awk -F": " '{print $2}' | sed '/^\s*$/d' )
+picture_height=$( ${sips} -g "pixelHeight" ${orig_dir}/* | awk -F": " '{print $2}' | sed '/^\s*$/d' )
+
+cp ${orig_dir}/* ${res_dir}/${res_dir}_000.jpg
+
+if [ ${picture_width} -ge ${picture_height} ]; then
+    $sips -Z 1024 ${res_dir}/${res_dir}_000.jpg
+else
+    $sips -Z 768 ${res_dir}/${res_dir}_000.jpg
+fi
+
+echo "done"
+
+exit
